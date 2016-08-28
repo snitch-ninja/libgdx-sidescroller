77df3a2f88417a935048ee91711187daa8e8bb81
snitch-ninja <justin.hunt3@yahoo.com>
2016-08-28 15:36:58 -0700

    initial commit

---
+++ desktop/src/com/test/test/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.test.test.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.test.test.TestGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TestGame(), config);
+	}
+}
