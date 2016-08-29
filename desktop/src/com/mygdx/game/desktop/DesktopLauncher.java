f68c2d1f80bd4dcbec2df3b05ddf05ea6285d6b7
snitch-ninja <justin.hunt3@yahoo.com>
2016-08-28 22:31:05 -0700

    update for newer tools

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
-package com.test.test.desktop;
+package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.test.test.TestGame;
+import com.mygdx.game.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new TestGame(), config);
+		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
